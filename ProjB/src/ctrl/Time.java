8d4cc07ea64cb213f0e7adb742fbe70769d2dca6
daniely9 <danielyakubov92@gmail.com>
2018-09-20 12:11:50 -0400

    Add files via upload

---
+++ ProjB/src/ctrl/Time.java
@@ -0,0 +1,73 @@
+package ctrl;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Brain;
+
+/**
+ * Servlet implementation class Time
+ */
+@WebServlet("/Time.do")
+public class Time extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Time() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		if (request.getParameter("calc") == null)
+		  {
+		     this.getServletContext().getRequestDispatcher("/Time.html").forward(request, response);
+		  }
+		  else
+		  {
+		     Brain model = new Brain();
+		     try
+		     {
+		        String time = model.doTime();
+		        response.setContentType("text/html");
+		        Writer out = response.getWriter();
+		        String html = "<html><body>";
+		        html += "<p><a href='Dash.do'>Back to Dashboard</a></p>";
+		        html += "<p>Server Time: " + time + "</p>";
+		        html += "</body></html>";
+		        out.write(html);
+		     }
+		     catch (Exception e)
+		     {
+		        response.setContentType("text/html");
+		        Writer out = response.getWriter();
+		        String html = "<html><body>";
+		        html += "<p><a href=' Time.do'>Back to Dashboard</a></p>";
+		        html += "<p>Error " + e.getMessage() + "</p>";
+		        out.write(html);
+		     }
+		  }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
