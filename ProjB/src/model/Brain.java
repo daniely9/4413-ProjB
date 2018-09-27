5d78ce85e0b57551e42ac8a6998c95112c3d1164
daniely9 <danielyakubov92@gmail.com>
2018-09-27 14:09:30 -0400

    Add files via upload

--- ProjB/src/model/Brain.java
+++ ProjB/src/model/Brain.java
@@ -2,11 +2,16 @@ package model;
 
 import java.io.*;
 import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.Socket;
+import java.net.URL;
+import java.net.URLConnection;
 import java.net.UnknownHostException;
 import java.sql.*;
 import java.util.Date;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Brain
 {
@@ -56,5 +61,36 @@ public class Brain
 		reader.read();
 		return reader.readLine();
 		
+	}
+	
+	public String doDb(String itemNo) throws Exception {
+		Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+		Connection con = DriverManager.getConnection(DB_URL);
+		Statement s = con.createStatement();
+		s.executeUpdate("set schema roumani");
+		String query = "SELECT NAME, PRICE FROM ITEM WHERE NUMBER='"+itemNo+"'";//SQL query to obtain the NAME and PRICE of an item whose number is itemNo in a table ITEM
+		ResultSet r = s.executeQuery(query);
+		String result = "";
+		if (r.next())
+		{
+			result = "$" + r.getDouble("PRICE") + " - " + r.getString("NAME");
+		}
+		else
+		{
+			throw new Exception(itemNo + " not found!");
+		}
+		r.close(); s.close(); con.close();
+		return result;
+	}
+	
+	public String doHttp(String country,String request) throws Exception{
+		URL url = new URL("https://www.eecs.yorku.ca/~roumani/servers/4413/f18/World.cgi?country="+country+"&query="+request);
+		URLConnection connection = url.openConnection();
+		InputStream response = connection.getInputStream();
+		Scanner scanner = new Scanner(response);
+		String result = scanner.useDelimiter("\\A").next();
+		//System.out.println(country + " " + request);
+
+		return result;
 	}
 }
